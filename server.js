14e7d097978a09c3b6ce43daa23a89be9b157c66
Alyssa Geria <alyssageria@gmail.com>
2023-04-10 19:13:39 -0700

    added notes to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// packages needed for the application
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -6,17 +7,21 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const { v4: uuidv4 } = require('uuid');
 
+// express app and port variable
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// reads json file and returns content as an array of notes
 const getNotes = () => {
     return readFile("db/db.json", "utf-8").then(notes => [].concat(JSON.parse(notes)))
 }
 
+// get the html files for the home and notes page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
@@ -25,10 +30,12 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+// return an array of notes in json format
 app.get('/api/notes', (req, res) => {
     getNotes().then(notes => res.json(notes)).catch(err => res.json(err))
 })
 
+// adds new notes to json file
 app.post('/api/notes', ({ body }, res) => {
     getNotes().then(oldNotes => {
         const newNotes = [...oldNotes, { title: body.title, text: body.text, id: uuidv4() }]
@@ -36,6 +43,7 @@ app.post('/api/notes', ({ body }, res) => {
     })
 });
 
+// route to delete notes
 app.delete('/api/notes/:id', (req, res) => {
     getNotes().then(oldNotes => {
         const newNotes = oldNotes.filter(note => note.id !== req.params.id)
@@ -43,10 +51,12 @@ app.delete('/api/notes/:id', (req, res) => {
     })
 })
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/index.html'))
-// })
+//catch all route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
+// server listener
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`)
 });
